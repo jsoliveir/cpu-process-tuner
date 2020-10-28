fb6bdbfdbda615666b14d5c650ff26ec088d759a
José Oliveira <jose.oliveira@sbanken.no>
2020-10-28 15:34:34 +0000

    fixing performance issues

--- .src/Public/Set-ProcessRules.ps1
+++ .src/Public/Set-ProcessRules.ps1
@@ -2,24 +2,26 @@
 Function Set-ProcessRules{
     param([Parameter(Mandatory = $true, ValueFromPipeline = $true)] $rules)
 
-    PROCESS{
+    BEGIN {
+        Write-Host "Setting rules ..."
         $processes =  Get-Process
+    }
+
+    PROCESS{
         foreach($r in $rules){
             $process =  ($processes | Where-Object Path -match $r.Selector);
             $process | Foreach-Object {
                 try{
                     $_.ProcessorAffinity = [int][cores]$r.CpuAffinity
-                    $_.PriorityClass = [string]$r.CpuPriority                
+                    $_.PriorityClass = [string]$r.CpuPriority  
+                    Get-Process | Select-Object `
+                    @{n="Process"; e={$_.Name}}, `
+                    @{n="Priority"; e={$_.PriorityClass}},  `
+                    @{n="Affinity"; e={[cores][int]$_.ProcessorAffinity}}
                 }catch{
+                    Write-Warning $_.Exception.Message
                 }
             }
         }     
-    }
-    
-    END {
-        Get-Process | Select-Object `
-        @{n="Process"; e={$_.Name}}, `
-        @{n="Affinity"; e={[cores][int]$_.ProcessorAffinity}}, `
-        @{n="Priority"; e={$_.PriorityClass}}
     }
 }
