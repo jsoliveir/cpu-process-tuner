9f89a2c1eb62bd76a679fe02019ef22cfc74f04f
José Oliveira <jose.oliveira@sbanken.no>
2020-10-28 15:44:15 +0000

    fixing performance issues

--- .src/Public/Set-ProcessRules.ps1
+++ .src/Public/Set-ProcessRules.ps1
@@ -8,19 +8,21 @@ Function Set-ProcessRules{
 
     PROCESS{
         foreach($r in $rules){
+            $r
             $process =  (Get-Process | Where-Object Path -match $r.Selector);
             $process | Foreach-Object {
                 try{
                     $_.ProcessorAffinity = [int][cores]$r.CpuAffinity
-                    $_.PriorityClass = [string]$r.CpuPriority  
-                    Get-Process | Select-Object `
-                    @{n="Process"; e={$_.Name}}, `
-                    @{n="Priority"; e={$_.PriorityClass}},  `
-                    @{n="Affinity"; e={[cores][int]$_.ProcessorAffinity}}
+                    $_.PriorityClass = [string]$r.CpuPriority                    
                 }catch{
-                    Write-Warning $_.Exception.Message
                 }
             }
         }     
+    }
+    END {
+        Get-Process | Select-Object `
+        @{n="Process"; e={$_.Name}}, `
+        @{n="Priority"; e={$_.PriorityClass}},  `
+        @{n="Affinity"; e={[cores][int]$_.ProcessorAffinity}}
     }
 }
