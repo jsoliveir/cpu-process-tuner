12b8bcc718f97c74795c48a21c9733f9a4553f73
Oliveira, José <Jose.Oliveira@sbanken.no>
2021-02-23 21:53:52 +0000

    fixing unit tests

--- Run-Tests.ps1
+++ Run-Tests.ps1
@@ -1,5 +1,5 @@
 Install-Module Pester -Force -SkipPublisherCheck
-Install-Module powershell-yaml
+Install-Module powershell-yaml -Force -SkipPublisherChe
 Import-Module Pester -MinimumVersion 5.0.4 -Force
 $ErrorActionPreference = "Stop"
 Invoke-Pester
