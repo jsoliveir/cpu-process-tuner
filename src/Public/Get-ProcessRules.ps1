16d8def46c02ac47002b5d6981c607fdc5934568
Oliveira, José <Jose.Oliveira@sbanken.no>
2021-02-25 23:22:38 +0000

    error handling

--- src/Public/Get-ProcessRules.ps1
+++ src/Public/Get-ProcessRules.ps1
@@ -1,7 +1,15 @@
 
 Function Get-ProcessRules{
     param([Parameter(Mandatory=$false)] $Config = (Get-ProcessConfigFile))
-    $RulesYml = (Get-Content $Config | ConvertFrom-Yaml).rules
+    
+    $RulesYml = (Get-Content $Config -ErrorAction SilentlyContinue | ConvertFrom-Yaml).rules
+
+    if( ! $RulesYml ){
+        Write-Warning "No Rules found!"
+        Write-Host -ForegroundColor DarkGray "Run [New-ProcessRule] to create the fist rule"
+        return
+    }
+
 
     $Rules = @()
 
