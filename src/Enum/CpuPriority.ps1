44fd932c1c7d43e9c12683fc4168a7345ad9946a
José Oliveira <36452207+jsoliveir@users.noreply.github.com>
2021-07-21 10:49:31 +0100

    Update CpuPriority.ps1

--- src/Enum/CpuPriority.ps1
+++ src/Enum/CpuPriority.ps1
@@ -1,8 +1,8 @@
 [flags()] Enum CpuPriority {
-    Idle       
-    BelowNormal
-    Normal     
-    AboveNormal
-    High       
-    RealTime   
+    Idle            = 0      
+    BelowNormal     = 1
+    Normal          = 2
+    AboveNormal     = 3
+    High            = 4
+    RealTime        = 5
 }
